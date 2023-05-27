e000f302272823901b489f0b55a9a78374ecf346
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-05-27 18:16:44 -0400

    update

--- crud/src/libs/MongoConnect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectMongoDB = async () => {
-    if(mongoose.connection.readyState ===1){
-        return mongoose.connection.asPromise()
-    }
-
-    return await mongoose.connect(process.env.MONGO_URI)
-}
---
+++ social/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+
+
+    </div>
+  )
+}
